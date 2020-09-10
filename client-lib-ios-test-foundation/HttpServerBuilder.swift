919e505782a93fea483bd8bbfb43abaca2d3efa0
Chiarotto Alessandro <alessandro.chiarotto.dechit@skytv.it>
2020-09-10 12:22:30 +0200

    init

---
+++ client-lib-ios-test-foundation/HttpServerBuilder.swift
@@ -0,0 +1,9 @@
+//
+//  HttpServerBuilder.swift
+//  client-lib-ios-test-foundation
+//
+//  Created by Sky on 10/09/2020.
+//  Copyright © 2020 Sky. All rights reserved.
+//
+
+import Foundation
---
+++ ios-test-foundation/ios-test-foundation/ios_test_foundation.h
@@ -0,0 +1,19 @@
+//
+//  ios_test_foundation.h
+//  ios-test-foundation
+//
+//  Created by Sky on 10/09/2020.
+//  Copyright © 2020 Sky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ios_test_foundation.
+FOUNDATION_EXPORT double ios_test_foundationVersionNumber;
+
+//! Project version string for ios_test_foundation.
+FOUNDATION_EXPORT const unsigned char ios_test_foundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ios_test_foundation/PublicHeader.h>
+
+
