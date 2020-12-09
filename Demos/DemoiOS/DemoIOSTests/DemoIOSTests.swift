2bbf7c0bd861f1eb08612889c50d407da2b0c002
Chiarotto Alessandro <alessandro.chiarotto.dechit@skytv.it>
2020-12-09 15:10:53 +0100

    Added Demo iOS

---
+++ Demos/DemoiOS/DemoIOSTests/DemoIOSTests.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import DemoIOS
+
+class DemoIOSTests: XCTestCase {
+
+}
---
+++ Demos/DemoiOS/DemoiOS/ImageLoader.swift
@@ -0,0 +1,29 @@
+import Foundation
+import SwiftUI
+import Combine
+
+class ImageLoader: ObservableObject {
+    @Published var image: UIImage?
+    private let url: URL
+    private var cancellable: AnyCancellable?
+
+    init(url: URL) {
+        self.url = url
+    }
+
+    deinit {
+        cancel()
+    }
+
+    func load() {
+        cancellable = URLSession.shared.dataTaskPublisher(for: url)
+            .map { UIImage(data: $0.data) }
+            .replaceError(with: nil)
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] in self?.image = $0 }
+    }
+
+    func cancel() {
+        cancellable?.cancel()
+    }
+}
