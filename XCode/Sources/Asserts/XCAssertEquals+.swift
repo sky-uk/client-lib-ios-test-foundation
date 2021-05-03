09a59a3247e4768b3b4ae94046a920d94fcd87e3
Chiarotto Alessandro <alessandro.chiarotto.dechit@skytv.it>
2021-05-03 16:51:46 +0200

    Added custom assert

--- XCode/Sources/Asserts/XCAssertEquals+.swift
+++ XCode/Sources/Asserts/XCAssertEquals+.swift
@@ -44,3 +44,26 @@ public func assertURLEquals(_ url1: String, _ url2: String, ignores: [URLCompone
         }
     }
 }
+
+
+public func assertEquals<T>(_ expression1: @autoclosure () throws -> T,
+                            _ expression2: @autoclosure () throws -> T,
+                            _ message: @autoclosure () -> String = "", file: StaticString = #filePath, line: UInt = #line) where T : Equatable {
+    return XCTAssertEqual(try expression1(), try expression2(), message(), file: file, line: line)
+}
+
+public func assertEquals<T>(_ expression1: @autoclosure () throws -> T, _ expression2: @autoclosure () throws -> T, accuracy: T, _ message: @autoclosure () -> String = "", file: StaticString = #filePath, line: UInt = #line) where T : FloatingPoint {
+    return XCTAssertEqual(try expression1(), try expression2(), accuracy: accuracy, message(), file: file, line: line)
+}
+
+public func assertEquals<T>(_ expression1: @autoclosure () throws -> T, _ expression2: @autoclosure () throws -> T, accuracy: T, _ message: @autoclosure () -> String = "", file: StaticString = #filePath, line: UInt = #line) where T : Numeric {
+    return XCTAssertEqual(try expression1(), try expression2(), accuracy: accuracy, message(), file: file, line: line)
+}
+
+public func assertNotNull(_ expression: @autoclosure () throws -> Any?, _ message: @autoclosure () -> String = "", file: StaticString = #filePath, line: UInt = #line) {
+    return XCTAssertNotNil(try expression(), message(), file: file, line: line)
+}
+
+public func assertNull(_ expression: @autoclosure () throws -> Any?, _ message: @autoclosure () -> String = "", file: StaticString = #filePath, line: UInt = #line) {
+    return XCTAssertNil(try expression(), message(), file: file, line: line)
+}
