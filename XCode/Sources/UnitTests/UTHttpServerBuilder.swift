c5d8a2f016cde774e8b39f8894452bee08ca772f
Chiarotto Alessandro <alessandro.chiarotto.dechit@skytv.it>
2021-09-13 14:32:20 +0200

    added log to ut mock server

--- XCode/Sources/UnitTests/UTHttpServerBuilder.swift
+++ XCode/Sources/UnitTests/UTHttpServerBuilder.swift
@@ -3,15 +3,16 @@ import XCTest
 import Swifter
 public class UTHttpServerBuilder {
     public private(set) var httpServer: HttpServer = HttpServer()
-    public var httpRoutes: [Route] = []
 
     public func route(_ endpoint: String, _ completion: @escaping (HttpRequest, Int) -> (HttpResponse)) -> UTHttpServerBuilder {
         let lock = DispatchSemaphore(value: 1)
+        Logger.info("Building endpoint: \(endpoint)")
         var callCount = 0
         httpServer.self[endpoint] = { request in
             lock.wait()
             callCount += 1
             lock.signal()
+            Logger.info("Handled request:\(request.method) \(request.path) Params:\(request.queryParams) call count:\(callCount)")
             return completion(request.httpRequest(), callCount)
         }
         return self
@@ -29,9 +30,6 @@ public class UTHttpServerBuilder {
 
     @discardableResult
     public func buildAndStart(port: in_port_t = 8080, forceIPv4: Bool = false, priority: DispatchQoS.QoSClass = .userInteractive) -> HttpServer {
-        httpRoutes.forEach { (route) in
-            buildRoute(endpoint: route.enpoint, completion: route.completion)
-        }
         do {
             try httpServer.start(port, forceIPv4: forceIPv4, priority: priority)
         } catch {
@@ -41,6 +39,7 @@ public class UTHttpServerBuilder {
     }
 
     private func buildRoute(endpoint: String, completion: @escaping (HttpRequest, Int) -> (HttpResponse)) {
+        Logger.info("Building endpoint: \(endpoint).")
         let lock = DispatchSemaphore(value: 1)
         var callCount = 0
         httpServer.self[endpoint] = { request in
