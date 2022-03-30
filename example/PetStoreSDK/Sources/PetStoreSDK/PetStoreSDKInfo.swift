a4a066fdea7bfe05890363645339178e0b1b6f34
Aleksandar Gotev <alex@gotev.net>
2022-03-30 14:44:01 +0200

    feat: faster switching between specs

--- example/PetStoreSDK/Sources/PetStoreSDK/PetStoreSDKInfo.swift
+++ example/PetStoreSDK/Sources/PetStoreSDK/PetStoreSDKInfo.swift
@@ -2,5 +2,5 @@ import Foundation
 
 public struct PetStoreSDKInfo {
     public static let version = "1.0.0"
-    public static let generationDateTime = "Sun Mar 27 16:10:53 CEST 2022"
+    public static let generationDateTime = "Wed Mar 30 14:43:05 CEST 2022"
 }
